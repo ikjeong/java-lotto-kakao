85069470fbc8d948d1ae6df406ed674ad8597b24
ikjeong <jipark7937@gmail.com>
2026-02-10 18:30:33 +0900

    feat: 수익률 계산 및 각 Rank별 당첨 개수 구현 및 테스트 작성

---
+++ src/main/java/lotto/model/LottoResult.java
@@ -0,0 +1,24 @@
+package lotto.model;
+
+import java.util.List;
+
+import lotto.LottoConfig;
+
+public class LottoResult {
+
+	private final List<Rank> ranks;
+
+	public LottoResult(List<Rank> ranks) {
+		this.ranks = ranks;
+	}
+
+	public Double calculateReturnRate() {
+		Integer sumPrize = ranks.stream().map(Rank::prize).reduce(0, Integer::sum);
+		Integer pay = LottoConfig.LOTTO_TICKET_PRICE * ranks.size();
+		return sumPrize.doubleValue() / pay;
+	}
+
+	public Integer countRank(Rank targetRank) {
+		return Math.toIntExact(ranks.stream().filter(targetRank::equals).count());
+	}
+}
