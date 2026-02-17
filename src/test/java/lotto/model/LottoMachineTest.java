a73a2f776ac46de33a1a3848082317a50905fbe3
ikjeong <jipark7937@gmail.com>
2026-02-17 17:36:18 +0900

    refactor: 테스트 공통 전처리 부분을 setup 함수로 추출

--- src/test/java/lotto/model/LottoMachineTest.java
+++ src/test/java/lotto/model/LottoMachineTest.java
@@ -1,20 +1,27 @@
 package lotto.model;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
-import java.util.List;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class LottoMachineTest {
 
+	int ticketPrice = 1_000;
+	LottoMachine lottoMachine;
+
+	@BeforeEach
+	void setup() {
+		LottoTicketRandomGenerator lottoTicketRandomGenerator = new LottoTicketRandomGenerator();
+		lottoMachine = new LottoMachine(new Money(ticketPrice), lottoTicketRandomGenerator);
+	}
+
 	@Test
 	@DisplayName("잔액 증가 테스트")
 	void validateDeposit() {
 		int purchasePrice = 1_500;
-		LottoTicketRandomGenerator lottoTicketRandomGenerator = new LottoTicketRandomGenerator();
-		LottoMachine lottoMachine = new LottoMachine(new Money(1_000), lottoTicketRandomGenerator);
 
 		lottoMachine.deposit(new Money(purchasePrice));
 		assertThat(lottoMachine.getBalance()).isEqualTo(new Money(purchasePrice));
@@ -25,26 +32,22 @@ public class LottoMachineTest {
 	@Test
 	@DisplayName("구매 가격에 따른 티켓 발행 개수 및 잔액 확인")
 	void validateLottoTicketCountByPurchasePrice() {
-		int ticketPrice = 1_000;
 		Money purchasePrice = new Money(ticketPrice * 14 + (ticketPrice-1));
-		LottoTicketRandomGenerator lottoTicketRandomGenerator = new LottoTicketRandomGenerator();
-		LottoMachine lottoMachine = new LottoMachine(new Money(ticketPrice), lottoTicketRandomGenerator);
 
 		lottoMachine.deposit(purchasePrice);
 		LottoMachineGeneratedResult machineGeneratedResult = lottoMachine.generate();
-		assertThat(machineGeneratedResult.totalPrice()).isEqualTo(new Money(ticketPrice * 14));
-		assertThat(machineGeneratedResult.lottoTickets().size()).isEqualTo(14);
+		Money totalPrice = machineGeneratedResult.totalPrice();
+		int buyLottoTicketNumber = machineGeneratedResult.lottoTickets().size();
 		Money balance = lottoMachine.getBalance();
-		assertThat(balance).isEqualTo(new Money(purchasePrice.amount() - machineGeneratedResult.totalPrice().amount()));
+		assertThat(totalPrice).isEqualTo(new Money(ticketPrice * 14));
+		assertThat(buyLottoTicketNumber).isEqualTo(14);
+		assertThat(balance).isEqualTo(new Money(purchasePrice.amount() - totalPrice.amount()));
 	}
 
 	@Test
 	@DisplayName("티켓 최소 구매 금액 미만 예외 처리")
 	void validateMinimumPurchasePrice() {
-		int ticketPrice = 1_000;
 		Money purchasePrice = new Money(ticketPrice-1);
-		LottoTicketRandomGenerator lottoTicketRandomGenerator = new LottoTicketRandomGenerator();
-		LottoMachine lottoMachine = new LottoMachine(new Money(ticketPrice), lottoTicketRandomGenerator);
 
 		lottoMachine.deposit(purchasePrice);
 		assertThatIllegalArgumentException().isThrownBy(() -> {
