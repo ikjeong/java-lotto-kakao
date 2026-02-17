36fdd40cc5bf365ff9d6c3b9e4387a5e4933ec3f
ikjeong <jipark7937@gmail.com>
2026-02-17 20:00:54 +0900

    feat: 수동 번호 로또 티켓 구매 기능 구현 및 테스트

--- src/main/java/lotto/model/LottoMachine.java
+++ src/main/java/lotto/model/LottoMachine.java
@@ -24,10 +24,20 @@ public class LottoMachine {
 	}
 
 	public PurchasedTickets purchaseAutoTickets() {
-		int price = balance.amount();
-		int ticketCount = price / lottoTicketPrice.amount();
+		int ticketCount = balance.amount() / lottoTicketPrice.amount();
 		Money totalPrice = new Money(lottoTicketPrice.amount() * ticketCount);
 		List<LottoTicket> lottoTickets = lottoTicketRandomGenerator.generate(ticketCount);
+		balance = new Money(balance.amount() - totalPrice.amount());
+		return new PurchasedTickets(totalPrice, lottoTickets);
+	}
+
+	public PurchasedTickets purchaseManualTicket(List<LottoNumber> targetLottoNumbers) {
+		if (balance.amount() < lottoTicketPrice.amount()) {
+			throw new IllegalArgumentException("금액이 부족하여 수동으로 로또를 구매할 수 없습니다.");
+		}
+
+		Money totalPrice = new Money(lottoTicketPrice.amount());
+		List<LottoTicket> lottoTickets = List.of(new LottoTicket(targetLottoNumbers));
 		balance = new Money(balance.amount() - totalPrice.amount());
 		return new PurchasedTickets(totalPrice, lottoTickets);
 	}
--- src/test/java/lotto/model/LottoMachineTest.java
+++ src/test/java/lotto/model/LottoMachineTest.java
@@ -1,6 +1,8 @@
 package lotto.model;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -32,11 +34,11 @@ public class LottoMachineTest {
 	@DisplayName("구매 가격에 따른 티켓 발행 개수 및 잔액 확인")
 	void validateLottoTicketCountByPurchasePrice() {
 		Money purchasePrice = new Money(ticketPrice * 14 + (ticketPrice-1));
-
 		lottoMachine.deposit(purchasePrice);
-		PurchasedTickets machineGeneratedResult = lottoMachine.purchaseAutoTickets();
-		Money totalPrice = machineGeneratedResult.totalPrice();
-		int buyLottoTicketNumber = machineGeneratedResult.lottoTickets().size();
+
+		PurchasedTickets purchasedTickets = lottoMachine.purchaseAutoTickets();
+		Money totalPrice = purchasedTickets.totalPrice();
+		int buyLottoTicketNumber = purchasedTickets.lottoTickets().size();
 		Money balance = lottoMachine.getBalance();
 		assertThat(totalPrice).isEqualTo(new Money(ticketPrice * 14));
 		assertThat(buyLottoTicketNumber).isEqualTo(14);
@@ -47,14 +49,60 @@ public class LottoMachineTest {
 	@DisplayName("티켓 최소 구매 금액 미만인 경우 티켓 발행 개수 및 잔액 확인")
 	void validateMinimumPurchasePrice() {
 		Money purchasePrice = new Money(ticketPrice-1);
-
 		lottoMachine.deposit(purchasePrice);
-		PurchasedTickets machineGeneratedResult = lottoMachine.purchaseAutoTickets();
-		Money totalPrice = machineGeneratedResult.totalPrice();
-		int buyLottoTicketNumber = machineGeneratedResult.lottoTickets().size();
+
+		PurchasedTickets purchasedTickets = lottoMachine.purchaseAutoTickets();
+		Money totalPrice = purchasedTickets.totalPrice();
+		int buyLottoTicketNumber = purchasedTickets.lottoTickets().size();
 		Money balance = lottoMachine.getBalance();
 		assertThat(totalPrice).isEqualTo(new Money(0));
 		assertThat(buyLottoTicketNumber).isEqualTo(0);
 		assertThat(balance).isEqualTo(new Money(purchasePrice.amount()));
+	}
+
+	@Test
+	@DisplayName("수동 티켓 구매 검증 및 잔액 확인")
+	void validatePurchaseManualTicket() {
+		Money purchasePrice = new Money(ticketPrice);
+		lottoMachine.deposit(purchasePrice);
+		List<LottoNumber> targetLottoNumbers = List.of(
+				LottoNumber.of(1),
+				LottoNumber.of(2),
+				LottoNumber.of(3),
+				LottoNumber.of(4),
+				LottoNumber.of(5),
+				LottoNumber.of(6)
+		);
+
+		PurchasedTickets purchasedTickets = lottoMachine.purchaseManualTicket(targetLottoNumbers);
+		Money totalPrice = purchasedTickets.totalPrice();
+		List<LottoTicket> lottoTickets = purchasedTickets.lottoTickets();
+		Money balance = lottoMachine.getBalance();
+		assertThat(totalPrice).isEqualTo(new Money(ticketPrice));
+		assertThat(lottoTickets.size()).isEqualTo(1);
+		assertThat(balance).isEqualTo(new Money(0));
+		List<LottoNumber> lottoNumbers = lottoTickets.getFirst().getSortedLottoNumbers();
+		for (int i = 0; i < 6; i++) {
+			assertThat(lottoNumbers.get(i)).isEqualTo(targetLottoNumbers.get(i));
+		}
+	}
+
+	@Test
+	@DisplayName("잔액 부족한 경우 수동 티켓 구매 예외 발생")
+	void validatePurchaseManualTicketWithInsufficientBalance() {
+		Money purchasePrice = new Money(ticketPrice-1);
+		lottoMachine.deposit(purchasePrice);
+		List<LottoNumber> targetLottoNumbers = List.of(
+				LottoNumber.of(1),
+				LottoNumber.of(2),
+				LottoNumber.of(3),
+				LottoNumber.of(4),
+				LottoNumber.of(5),
+				LottoNumber.of(6)
+		);
+
+		assertThatIllegalArgumentException().isThrownBy(() -> {
+			PurchasedTickets purchasedTickets = lottoMachine.purchaseManualTicket(targetLottoNumbers);
+		});
 	}
 }
